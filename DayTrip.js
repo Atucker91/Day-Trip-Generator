a311a44e46e6a542d23b25ca54c72c84bb9715cb
Atucker91 <89651874+Atucker91@users.noreply.github.com>
2021-08-30 16:37:11 -0500

    Added verification for switch statement

--- DayTrip.js
+++ DayTrip.js
@@ -5,9 +5,9 @@
 
 
 
-let des = ["Seattle", "New York", "Austin", "Chicago", "Atlanta"];
-let rest = ["Italian", "Thai", "TexMex", "Greek", "American"];
-let trans = ["Plane", "Train", "Car", "Bike", "Scooter"];
+let des = ["Seattle,", "New York,", "Austin,", "Chicago,", "Atlanta,"];
+let rest = ["Italian,", "Thai,", "TexMex,", "Greek,", "American,"];
+let trans = ["Plane,", "Train,", "Car,", "Bike,", "Scooter,"];
 let ent = ["Concert", "Sporting Event", "Comedy Show", "Musical", "Art Exhibit"];
 let tripList = [];
 let tripListAsString;
@@ -28,27 +28,32 @@ while (userChoice === "Yes") {
 
     let option = prompt("Enter 0 for Destination, 1 for Restaurant, 2 for Transportation, and 3 for Entertainment");
 
-    switch (option) {
-        case "0":
-            tripList[0] = randGenerator(des);
-            break;
-        case "1":
-            tripList[1] = randGenerator(rest);
-            break;
-        case "2":
-            tripList[2] = randGenerator(trans);
-            break;
-        case "3":
-            tripList[3] = randGenerator(ent);
-            break;
-        default:
-            option = prompt("Invalid choice, try again");
-            break;
+    if (option === "0" || option === "1" || option === "2" || option === "3") {
+
+        switch (option) {
+            case "0":
+                tripList[0] = randGenerator(des);
+                break;
+            case "1":
+                tripList[1] = randGenerator(rest);
+                break;
+            case "2":
+                tripList[2] = randGenerator(trans);
+                break;
+            case "3":
+                tripList[3] = randGenerator(ent);
+                break;
+        }
+
+        tripListAsString = makeIntoString(tripList);
+        console.log(tripListAsString);
+        userChoice = prompt("Would you like to re-generate a selction? Enter Yes or No");
     }
 
-    tripListAsString = makeIntoString(tripList);
-    console.log(tripListAsString);
-    userChoice = prompt("Would you like to re-generate a selction? Enter Yes or No");
+    else {
+
+        console.log("Invalid choice, try again");
+    }
 }
 
 console.log("Your day trip is as follows: " + tripListAsString);
